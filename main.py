2faaa0b96a889bfa0fdba4a0d33da567d7c60987
Ariel Vera <avera@charlotte.edu>
2025-02-12 22:16:04 -0500

    feat: added 2 subclasses (Savings & Checking) that inherit from Bank Account

--- main.py
+++ main.py
@@ -12,36 +12,55 @@ class BankAccount:
     def withdraw(self, amount):
         new_balance = self.current_balance - amount
         if new_balance < self.minimum_balance:
-            print(f"ERROR: {self.customer_name} cannot withdraw ${amount} due to their minimum balance threshold!\n")
+            print(f"[ERROR] {self.customer_name} cannot withdraw ${amount} due to their minimum balance threshold!\n")
+            return False
         else:
             self.current_balance = new_balance
+        return True
 
     def print_customer_information(self):
         print(f"Title of Bank: {self.title}\n"
               f"Customer Name: {self.customer_name}\n"
               f"Current Balance: {self.current_balance}\n"
-              f"Minimum Balance: {self.minimum_balance}\n")
+              f"Minimum Balance: {self.minimum_balance}")
 
 class Savings(BankAccount):
-    def __init__(self, customer_name, current_balance, minimum_balance):
+    def __init__(self, customer_name, current_balance, minimum_balance, interest):
         super().__init__(customer_name, current_balance, minimum_balance)
+        self.interest = interest
+
+    def apply_interest(self):
+        self.deposit(self.current_balance * self.interest)
+
+    def print_customer_information(self):
+        super().print_customer_information()
+        print(f"Interest Rate: {self.interest}\n")
 
 class Checking(BankAccount):
-    def __init__(self, customer_name, current_balance, minimum_balance):
+    def __init__(self, customer_name, current_balance, minimum_balance, transfer_limit):
         super().__init__(customer_name, current_balance, minimum_balance)
+        self.transfer_limit = transfer_limit
 
-ba1 = BankAccount("Ariel Vera", 100, 25)
+    def apply_transfer(self, account, amount):
+        if amount <= self.transfer_limit:
+            if self.withdraw(amount):
+                account.deposit(amount)
+                print(f"[SUCCESS] {self.customer_name} sent ${amount} to {account.customer_name}!\n")
+        else:
+            print(f"[ERROR] {self.customer_name} cannot send money to {account.customer_name} as they have exceeded their transfer limit!\n")
+
+    def print_customer_information(self):
+        super().print_customer_information()
+        print(f"Transfer Limit: {self.transfer_limit}\n")
+
+ba1 = Savings("Ariel Vera", 100, 25, 0.5)
 ba1.print_customer_information()
-ba1.deposit(100)
-ba1.withdraw(200)
-ba1.deposit(200)
-ba1.withdraw(100)
+ba1.apply_interest()
 ba1.print_customer_information()
 
-ba2 = BankAccount("Travis Marshall", 2500, 500)
+ba2 = Checking("Travis Marshall", 2500, 500, 500)
 ba2.print_customer_information()
-ba2.deposit(500)
-ba2.withdraw(1000)
-ba2.deposit(1000)
-ba2.withdraw(3000)
+ba2.apply_transfer(ba1, 350)
 ba2.print_customer_information()
+
+ba1.print_customer_information()
