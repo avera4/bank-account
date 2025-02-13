5a38795b1c98b032d47507338a1a72277bf74e7e
Ariel Vera <avera@charlotte.edu>
2025-02-12 22:52:46 -0500

    feat: moved classes to banking module. imported banking module classes to main file. illustrated scenarios in main file

---
+++ banking.py
@@ -0,0 +1,58 @@
+class BankAccount:
+    title = "Harvest & Trustee"
+
+    def __init__(self, customer_name, current_balance, minimum_balance, account_number, routing_number):
+        self.customer_name = customer_name
+        self.current_balance = current_balance
+        self.minimum_balance = minimum_balance
+        self.__account_number = account_number
+        self._routing_number = routing_number
+
+    def deposit(self, amount):
+        self.current_balance += amount
+
+    def withdraw(self, amount):
+        new_balance = self.current_balance - amount
+        if new_balance < self.minimum_balance:
+            print(f"[ERROR] {self.customer_name} cannot withdraw ${amount} due to their minimum balance threshold!\n")
+            return False
+        else:
+            self.current_balance = new_balance
+        return True
+
+    def print_customer_information(self):
+        print(f"Title of Bank: {self.title}\n"
+              f"Customer Name: {self.customer_name}\n"
+              f"Current Balance: {self.current_balance}\n"
+              f"Minimum Balance: {self.minimum_balance}\n"
+              f"Account Number: {self.__account_number}\n"
+              f"Routing Number: {self._routing_number}")
+
+class Savings(BankAccount):
+    def __init__(self, customer_name, current_balance, minimum_balance, account_number, routing_number, interest):
+        super().__init__(customer_name, current_balance, minimum_balance, account_number, routing_number)
+        self.interest = interest
+
+    def apply_interest(self):
+        self.deposit(self.current_balance * self.interest)
+
+    def print_customer_information(self):
+        super().print_customer_information()
+        print(f"Interest Rate: {self.interest}\n")
+
+class Checking(BankAccount):
+    def __init__(self, customer_name, current_balance, minimum_balance, account_number, routing_number, transfer_limit):
+        super().__init__(customer_name, current_balance, minimum_balance, account_number, routing_number)
+        self.transfer_limit = transfer_limit
+
+    def apply_transfer(self, account, amount):
+        if amount <= self.transfer_limit:
+            if self.withdraw(amount):
+                account.deposit(amount)
+                print(f"[SUCCESS] {self.customer_name} sent ${amount} to {account.customer_name}!\n")
+        else:
+            print(f"[ERROR] {self.customer_name} cannot send money to {account.customer_name} as they have exceeded their transfer limit!\n")
+
+    def print_customer_information(self):
+        super().print_customer_information()
+        print(f"Transfer Limit: {self.transfer_limit}\n")
